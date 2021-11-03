76e6125b64ca695ea021adc9f556377339e5789c
l33th <l33th@protonmail.ch>
2021-11-03 10:02:30 +0200

    added some links

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
   return (
     <div>
       <h1>About me</h1>
       <h2>I do front-end and back-end web development in Go and ReactJS.</h2>
+      <Link to="/contact">Contact</Link>
     </div>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const ContactPage = () => {
   return (
     <div>
       <h1>Contact me</h1>
-      <h2>The best way to contact me is via wmouton.go@gmail.com</h2>
+      <h2>The best way to contact me is via l33th@protonmail.ch on twitter</h2>
+      <p>
+        You can follow me on{' '}
+        <a href="https://twitter.com/l33th_dev" target="_blank">
+          Twitter
+        </a>
+      </p>
     </div>
   )
 }
