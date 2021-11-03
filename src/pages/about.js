c80ea675bcd4c619423afba073e8f167acc98803
l33th <l33th@protonmail.ch>
2021-11-03 09:42:59 +0200

    Blog and About Pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About me</h1>
+      <h2>I do front-end and back-end web development in Go and ReactJS.</h2>
+    </div>
+  )
+}
+
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
-import React from "react"
+import React from 'react'
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello.</h1>
+      <h2>I'm L33th, a full-stack web developer.</h2>
+    </div>
+  )
 }
+
+export default IndexPage
