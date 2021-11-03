512e3b74875e2a8fec273c54070274efec275e5d
l33th <l33th@protonmail.ch>
2021-11-03 09:47:37 +0200

    added contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact me</h1>
+      <h2>The best way to contact me is via wmouton.go@gmail.com</h2>
+    </div>
+  )
+}
+
+export default ContactPage
