14f770801f1c8ffa9e7292350af876ae909c2423
l33th <l33th@protonmail.ch>
2021-11-04 06:42:32 +0200

    added layout and props-children

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,17 @@ import React from 'react'
 // import link
 import { Link } from 'gatsby'
 // import components
-import Header from '../components/header'
-import Footer from '../components/footer'
+import Layout from '../components/layout'
 
 const IndexPage = () => {
   return (
-    <div>
-      <Header />
+    <Layout>
       <h1>Hello.</h1>
       <h2>I'm L33th, a full-stack web developer.</h2>
       <p>
         Need a developer? <Link to="/contact">Contact me</Link>
       </p>
-      <Footer />
-    </div>
+    </Layout>
   )
 }
 
