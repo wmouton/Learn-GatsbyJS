7c42d451aa415c6aeaf06058dd2929404839187d
l33th <l33th@protonmail.ch>
2021-11-04 06:04:02 +0200

    display footer on blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,13 @@
 import React from 'react'
+// import components
+import Footer from '../components/footer'
 
 const BlogPage = () => {
   return (
     <div>
       <h1>Blog</h1>
       <p>Posts will show up here later on.</p>
+      <Footer />
     </div>
   )
 }
