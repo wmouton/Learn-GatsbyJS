010964de8e23d7cef4af89b5a486c9f6f4c6591a
l33th <l33th@protonmail.ch>
2021-11-05 08:32:02 +0200

    added index.css to test

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,8 @@ import React from 'react'
 // import components
 import Header from './header'
 import Footer from './footer'
+// import css
+import '../styles/index.css'
 
 const Layout = ({ children }) => {
   return (
