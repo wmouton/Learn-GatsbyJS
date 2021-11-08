723085e1a618a561e575bf242c24614c73f88770
l33th <l33th@protonmail.ch>
2021-11-08 11:04:24 +0200

    iterate over slugs and display content

---
+++ src/templates/blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+const Blog = () => {
+  return (
+    <div>
+      <Layout>This is the blog template</Layout>
+    </div>
+  )
+}
+
+export default Blog
