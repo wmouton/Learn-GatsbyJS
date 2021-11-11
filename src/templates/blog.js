1d266af6ff9805a6f6628e7ae32c7a467877ef2d
l33th <l33th@protonmail.ch>
2021-11-11 06:07:39 +0200

    display blog posts when links are clicked

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,9 +1,28 @@
 import React from 'react'
+// imports from gatsby
+import { graphql } from 'gatsby'
+// import components
 import Layout from '../components/layout'
-const Blog = () => {
+
+export const query = graphql`
+  query ($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+// blog functional component
+const Blog = ({ data }) => {
   return (
     <div>
-      <Layout>This is the blog template</Layout>
+      <Layout>
+        <h1>{data.markdownRemark.frontmatter.title}</h1>
+      </Layout>
     </div>
   )
 }
